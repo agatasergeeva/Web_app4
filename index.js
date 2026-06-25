59b7eada5b5b830f80d17c153b438b4c8e78b3a4
Agata <agata@MacBook-Air-Agata.local>
2026-06-25 16:13:08 +0300

    tasks 1

--- index.js
+++ index.js
@@ -4,32 +4,61 @@ const app = express();
 
 const LOGIN = "agata_86";
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.type("text/plain").send("Web application is running");
+  res.setHeader("X-Author", LOGIN);
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+  res.send(LOGIN);
 });
 
-app.get("/login", (req, res) => {
-  res.type("text/plain").send(LOGIN);
+app.get("/login/", (req, res) => {
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+  res.send(LOGIN);
 });
 
-app.get("/id/:N", async (req, res) => {
-  try {
-    const N = req.params.N;
+app.get("/sample/", (req, res) => {
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+  res.send("function task(x){ return x * this * this; }");
+});
 
-    const response = await fetch(`https://nd.kodaktor.ru/users/${N}`, {
-      method: "GET"
+app.get("/promise/", (req, res) => {
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+  res.send("function task(x){ return x < 18 ? Promise.resolve('yes') : Promise.reject('no'); }");
+});
+
+app.get("/fetch/", (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=UTF-8");
+
+  res.send(`<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Fetch Task</title>
+</head>
+<body>
+  <input id="inp">
+  <button id="bt">Fetch</button>
+
+  <script>
+    const inp = document.getElementById("inp");
+    const bt = document.getElementById("bt");
+
+    bt.addEventListener("click", async () => {
+      const response = await fetch(inp.value);
+      const text = await response.text();
+      inp.value = text;
     });
-
-    const data = await response.json();
-
-    res.type("text/plain").send(data.login);
-  } catch (error) {
-    res.status(500).type("text/plain").send("Error");
-  }
+  </script>
+</body>
+</html>`);
 });
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
